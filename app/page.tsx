339fe76de4f920f7d75afdbd178f92619d223ba4
Hello <deftqml@gmail.com>
2023-07-14 21:07:34 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Ban Sombo, A content creator base in <i className="underline">Phnom Penh, Cambodia</i> 
+					Hi, my name is Ban Sambo, A content creator base in <i className="underline">Phnom Penh, Cambodia</i> 
 					<br />
 					learn more about me {" "}
 					<Link
